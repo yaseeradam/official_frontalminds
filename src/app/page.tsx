06e703b9251f7dcd330e45ff95e667678422fd18
Yaseer K Adam <princeykarg@gmail.com>
2025-09-24 22:13:28 +0000

    make it fully responsive for mobile and small screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,7 +103,7 @@ export default function Home() {
           </p>
         </div>
         <div
-          className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]"
+          className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_32px,_black_calc(100%-32px),transparent_100%)] md:[mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]"
         >
           <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-scroll-left">
             {techStack.map((tech) => {
